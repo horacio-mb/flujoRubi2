84cf87ce29908d5cd3855bd10d7e7471d4392c53
Horacio Molina <horaciomolinab0@gmail.com>
2021-03-02 19:49:13 -0400

    Muchos cambios

---
+++ apiProductor/Controllers/DataController.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace apiProductor.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Threading.Tasks;
+    using Azure.Messaging.ServiceBus;
+    using apiProductor.Models;
+    using Newtonsoft.Json;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<bool> EnviarAsync([FromBody] Data data)
+        {
+            string connectionString = "Endpoint=sb://queuehoracio.servicebus.windows.net/;SharedAccessKeyName=enviar;SharedAccessKey=SDC8MDPHqYiSJoZzlkS5bR/EHVyDWWMlTziLWmoTzMM=;EntityPath=cola1";
+            string queueName = "cola1";
+
+            string mensaje = JsonConvert.SerializeObject(data);
+
+            // create a Service Bus client 
+            await using (ServiceBusClient client = new ServiceBusClient(connectionString))
+            {
+                // create a sender for the queue 
+                ServiceBusSender sender = client.CreateSender(queueName);
+
+                // create a message that we can send
+                ServiceBusMessage message = new ServiceBusMessage(mensaje);
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {queueName}");
+            }
+            return true;
+        }
+    }
+}
---
+++ apiProductor/Models/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiProductor.Models
+{
+    public class Data
+    {
+        [Key]
+        public string NameDevice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime EventDate { get; set; }
+        [Required]
+        public string Event { get; set; }
+
+    }
+}
